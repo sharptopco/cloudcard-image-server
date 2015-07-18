786a4827f8dfa0c6b77156e0bdaaa60d7dc96f55
Tony Erskine <tony@sharptopsoftware.com>
2015-07-18 06:42:01 -0400

    added common project config

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -8,7 +8,6 @@ dataSource {
 hibernate {
     cache.use_second_level_cache = true
     cache.use_query_cache = false
-//    cache.region.factory_class = 'net.sf.ehcache.hibernate.EhCacheRegionFactory' // Hibernate 3
     cache.region.factory_class = 'org.hibernate.cache.ehcache.EhCacheRegionFactory' // Hibernate 4
     singleSession = true // configure OSIV singleSession mode
     flush.mode = 'manual' // OSIV session flush mode outside of transactional context
@@ -20,6 +19,10 @@ environments {
         dataSource {
             dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
             url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
+        }
+        dataSource_bbts {
+            dbCreate = "update"
+            url = "jdbc:h2:mem:bbtsDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
         }
     }
     test {
@@ -52,6 +55,10 @@ environments {
                 jdbcInterceptors = "ConnectionState"
                 defaultTransactionIsolation = java.sql.Connection.TRANSACTION_READ_COMMITTED
             }
+        }
+        dataSource_bbts {
+            dbCreate = "update"
+            url = "jdbc:h2:mem:bbtsDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
         }
     }
 }
